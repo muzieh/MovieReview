016ace310616bbe64a5823341813d48968cad669
Marcin Owerczuk <muzieh@gmail.com>
2015-07-04 00:15:29 +0100

    Context

---
+++ MovieReview.Data/MovieReviewDBContext.cs
@@ -0,0 +1,31 @@
+﻿using MovieReview.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace MovieReview.Data
+{
+    public class MovieReviewDBContext : DbContext
+    {
+        public MovieReviewDBContext() : base(nameOrConnectionString:"moviewsReviewProd") 
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MoviesReview> MoviesReviews { get; set;}
+
+        static MovieReviewDBContext()
+        {
+            Database.SetInitializer(new MoviewReviewDatabaseInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
---
+++ MovieReview.Data/MoviewReviewDatabaseInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace MovieReview.Data
+{
+    public class MoviewReviewDatabaseInitializer : DropCreateDatabaseIfModelChanges<MovieReviewDBContext>
+    {
+        protected override void Seed(MovieReviewDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
